be49caa985247215208a7263cf5d3b0d6b27dcbc
Adrian Hunter <adrian.hunter@intel.com>
2013-10-11 15:28:35 +0300

    perf tools: Hit all build ids when Instruction Tracing
    We need to include all buildids when a perf.data
    file contains Instruction Tracing data because we
    do not decode the trace for that purpose because
    it would take too long.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -683,8 +683,16 @@ out_child:
 	if (!err && !file->is_pipe) {
 		rec->session->header.data_size += rec->bytes_written;
 
-		if (!rec->no_buildid)
+		if (!rec->no_buildid) {
 			process_buildids(rec);
+			/*
+			 * We take all buildids when the file contains
+			 * Instruction Tracing data because we do not decode the
+			 * trace because it would take too long.
+			 */
+			if (record_opts__itracing(&rec->opts))
+				dsos__hit_all(rec->session);
+		}
 		perf_session__write_header(rec->session, rec->evlist,
 					   file->fd, true);
 	}
