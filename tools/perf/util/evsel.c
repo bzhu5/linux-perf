kernelorg-mirror/linux_kernel_git_ak_linux-misc

perf tools: Add Instruction Trace sampling support
Add functions to configure Instruction
Trace sampling and queue Instruction
Trace samples for processing

Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

tools/perf/util/evsel.c
@@ -639,6 +639,13 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts)
 	if (opts->sample_weight)
 		perf_evsel__set_sample_bit(evsel, WEIGHT);
 
+	if (opts->sample_itrace && !evsel->no_aux_samples) {
+		perf_evsel__set_sample_bit(evsel, ITRACE);
+		attr->itrace_config = opts->itrace_sample_config;
+		attr->itrace_sample_size = opts->itrace_sample_size;
+		attr->itrace_sample_type = opts->itrace_sample_type;
+	}
+
 	attr->mmap  = track;
 	attr->comm  = track;
 

bzhu5/linux-perf

tools/perf/util/evsel.c
@@ -662,6 +662,13 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts)
 	if (opts->sample_weight)
 		perf_evsel__set_sample_bit(evsel, WEIGHT);
 
+	if (opts->sample_itrace && !evsel->no_aux_samples) {
+		perf_evsel__set_sample_bit(evsel, AUX);
+		attr->aux_sample_config = opts->itrace_sample_config;
+		attr->aux_sample_size = opts->itrace_sample_size;
+		attr->aux_sample_type = opts->itrace_sample_type;
+	}
+
 	attr->mmap  = track;
 	attr->mmap2 = track && !perf_missing_features.mmap2;
 	attr->comm  = track;